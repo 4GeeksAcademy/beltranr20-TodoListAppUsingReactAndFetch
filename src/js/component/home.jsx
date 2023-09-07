058284a5eaef51c7ad73700d575d5c47eaec5451
Randy Beltran <131041473+Beltranr20@users.noreply.github.com>
2023-09-07 18:51:34 +0000

    fully functioning todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,87 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const[todos, setTodos] = useState([])
+	useEffect(()=>{
+		fetch("https://playground.4geeks.com/apis/fake/todos/user/beltranr20")
+		.then((response)=> response.json())
+		.then((data)=>{
+			console.log(data)
+			setTodos(data)
+		})
+	},[])
+	const createTodo=(e)=>{
+		// stopping default refresh action
+		e.preventDefault()
+		let updatedTodos = todos
+		let newTodo={
+			// declaring todo.label
+			"label" : e.target.todoInput.value, //from the event we're targeting the input and getting its value
+			"done" : false 
+		}
+		let isNew = true
+		todos.forEach((todo,index)=>{
+			if(todo.label.toLowerCase() === newTodo.label.toLowerCase()){
+				isNew = false
+			}
+		})
+		// isNew === true ? setTodos([...todos, newTodo]) : alert("Todo already exists.")
+		if(isNew===true){
+			updatedTodos.push(newTodo)
+			fetch("https://playground.4geeks.com/apis/fake/todos/user/beltranr20",{
+				method: "PUT",
+				body: JSON.stringify(
+					updatedTodos
+				),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+			.then((response)=> response.json())
+			.then((data)=>{
+				console.log(data)
+				setTodos([...updatedTodos])
+			})
+		}
+		e.target.todoInput.value = ""	
+	}
+	const removeTodo = (e,index)=>{
+		e.preventDefault()
+		let currentTodos = todos
+		let filterTodos = currentTodos.filter((item,idx)=> idx !== index)
+		// updating setTodos function and allows page refresh 
+		fetch("https://playground.4geeks.com/apis/fake/todos/user/beltranr20",{
+				method: "PUT",
+				body: JSON.stringify(
+					filterTodos
+				),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+			.then((response)=> response.json())
+			.then((data)=>{
+				console.log(data)
+				setTodos([...filterTodos])
+			}) 
+	}
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<form onSubmit={createTodo}> 
+				<input name="todoInput" type="text" placeholder="Enter a Todo"/>
+			</form>
+			<ul>
+				{todos.map((todo,index)=>{
+					return(
+						<li key={index}>
+							<span>{todo.label}</span>
+							<button onClick={(e)=>removeTodo(e,index)}>Delete</button>
+						</li>
+
+					)
+				})}
+			</ul>
 		</div>
 	);
 };
